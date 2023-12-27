a57ee7b10b7e929be5e8e0f0f8eb4bb2d02ebe1c
SATYAM SINGH <96167040+satyamsingh2001@users.noreply.github.com>
2023-12-27 11:25:49 +0530

    done

--- lib/screen/views/home_screen.dart
+++ lib/screen/views/home_screen.dart
@@ -46,213 +46,216 @@ class _HomeScreenState extends State<HomeScreen> {
               .copyWith(color: AppColors.white),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              children: [
-                GetBuilder<ImageController>(
-                  builder: (imageController) => InkWell(
-                    onTap: () async {
-                      await imageController.pickedProfile(ImageSource.gallery);
-                    },
-                    child: Container(
-                      width: 50,
-                      height: 50,
-                      decoration: BoxDecoration(
-                        color: AppColors.white,
-                        shape: BoxShape.circle,
-                        image: DecorationImage(
-                          fit: BoxFit.cover,
-                          image: imageController.profile != null
-                              ? FileImage(imageController.profile!)
-                              : const AssetImage(pic) as ImageProvider,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  GetBuilder<ImageController>(
+                    builder: (imageController) => InkWell(
+                      onTap: () async {
+                        await imageController.pickedProfile(ImageSource.gallery);
+                      },
+                      child: Container(
+                        width: 50,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          color: AppColors.white,
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: imageController.profile != null
+                                ? FileImage(imageController.profile!)
+                                : const AssetImage(pic) as ImageProvider,
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-                const SizedBox(
-                  width: 10,
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Satyam Singh",
-                      style: AppTextStyles.kBody17SemiboldTextStyle
-                          .copyWith(color: AppColors.white),
-                    ),
-                    Text(
-                      "Developer",
-                      style: AppTextStyles.kBody15RegularTextStyle
-                          .copyWith(color: AppColors.white),
-                    ),
-                  ],
-                )
-              ],
-            ),
-            ConstantContainer(
-              height: Get.height * .25,
-              width: Get.width,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    RichText(
-                      text: TextSpan(
-                        text: "How's the ",
-                        style: AppTextStyles.kBody20SemiboldTextStyle
-                            .copyWith(color: AppColors.white),
-                        children: <TextSpan>[
-                          TextSpan(
-                            text: 'Mood\n',
-                            style: AppTextStyles.kBody20SemiboldTextStyle
-                                .copyWith(color: AppColors.primary),
-                          ),
-                          TextSpan(
-                            text: 'Today?',
-                            style: AppTextStyles.kBody20SemiboldTextStyle
-                                .copyWith(color: AppColors.white),
-                          ),
-                        ],
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    SizedBox(
-                      height: Get.height * 0.1,
-                      width: Get.width,
-                      child: FutureBuilder<List<SimpleEmoji>>(
-                        future: futureEmojiList,
-                        builder: (context, snapshot) {
-                          if (snapshot.connectionState ==
-                              ConnectionState.waiting) {
-                            return const Center(
-                                child: CircularProgressIndicator());
-                          } else if (snapshot.hasError) {
-                            return Text('Error: ${snapshot.error}');
-                          } else {
-                            List<SimpleEmoji>? emojiList = snapshot.data;
-                            return ListView.builder(
-                              scrollDirection: Axis.horizontal,
-                              physics: const NeverScrollableScrollPhysics(),
-                              shrinkWrap: true,
-                              itemCount: 5,
-                              itemBuilder: (context, index) {
-                                SimpleEmoji emoji = emojiList![index];
-                                return Text(
-                                  emoji.character,
-                                  style: AppTextStyles.kLargeTitleTextStyle,
-                                );
-                              },
-                            );
-                          }
-                        },
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ),
-            const Divider(
-              color: AppColors.white,
-              thickness: 1.5,
-            ),
-            Row(
-              children: [
-                Text(
-                  SimpleEmoji(character: "😀").character,
-                  style: AppTextStyles.kHeading3TextStyle,
-                ),
-                Text(
-                  "  Team Mood  ",
-                  style: AppTextStyles.kBody17RegularTextStyle
-                      .copyWith(color: AppColors.white),
-                ),
-                const Expanded(
-                  child: Divider(
-                    color: AppColors.white,
-                    thickness: 1.5,
+                  const SizedBox(
+                    width: 10,
                   ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            ConstantContainer(
-                height: Get.height * .13,
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Satyam Singh",
+                        style: AppTextStyles.kBody17SemiboldTextStyle
+                            .copyWith(color: AppColors.white),
+                      ),
+                      Text(
+                        "Developer",
+                        style: AppTextStyles.kBody15RegularTextStyle
+                            .copyWith(color: AppColors.white),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              ConstantContainer(
+                height: Get.height * .25,
                 width: Get.width,
                 child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          RichText(
-                            text: TextSpan(
-                              text: ' " ',
-                              style: AppTextStyles.kHeading1TextStyle
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      RichText(
+                        text: TextSpan(
+                          text: "How's the ",
+                          style: AppTextStyles.kBody20SemiboldTextStyle
+                              .copyWith(color: AppColors.white),
+                          children: <TextSpan>[
+                            TextSpan(
+                              text: 'Mood\n',
+                              style: AppTextStyles.kBody20SemiboldTextStyle
                                   .copyWith(color: AppColors.primary),
-                              children: <TextSpan>[
-                                TextSpan(
-                                  text: 'The team is feeling good\n     today',
-                                  style: AppTextStyles.kBody20SemiboldTextStyle
-                                      .copyWith(color: AppColors.white),
-                                ),
-                                TextSpan(
-                                  text: ' "',
-                                  style: AppTextStyles.kHeading1TextStyle
-                                      .copyWith(color: AppColors.primary),
-                                ),
-                              ],
                             ),
-                          ),
-                        ]))),
-            const SizedBox(
-              height: 20,
-            ),
-            Expanded(
-              child: LineChart(
-                LineChartData(
-                  gridData: FlGridData(show: true),
-                  titlesData: FlTitlesData(show: true),
-                  borderData: FlBorderData(
-                    show: true,
-                    border: Border.all(color: AppColors.white90, width: 1),
+                            TextSpan(
+                              text: 'Today?',
+                              style: AppTextStyles.kBody20SemiboldTextStyle
+                                  .copyWith(color: AppColors.white),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      SizedBox(
+                        height: Get.height * 0.1,
+                        width: Get.width,
+                        child: FutureBuilder<List<SimpleEmoji>>(
+                          future: futureEmojiList,
+                          builder: (context, snapshot) {
+                            if (snapshot.connectionState ==
+                                ConnectionState.waiting) {
+                              return const Center(
+                                  child: CircularProgressIndicator());
+                            } else if (snapshot.hasError) {
+                              return Text('Error: ${snapshot.error}',style: const TextStyle(color: AppColors.white),);
+                            } else {
+                              List<SimpleEmoji>? emojiList = snapshot.data;
+                              return ListView.builder(
+                                scrollDirection: Axis.horizontal,
+                                physics: const NeverScrollableScrollPhysics(),
+                                shrinkWrap: true,
+                                itemCount: 5,
+                                itemBuilder: (context, index) {
+                                  SimpleEmoji emoji = emojiList![index];
+                                  return Text(
+                                    emoji.character,
+                                    style: AppTextStyles.kLargeTitleTextStyle,
+                                  );
+                                },
+                              );
+                            }
+                          },
+                        ),
+                      )
+                    ],
                   ),
-                  minX: 0,
-                  maxX: 9,
-                  minY: 0,
-                  maxY: 5,
-                  lineBarsData: [
-                    LineChartBarData(
-                      spots: [
-                        const FlSpot(0, 4),
-                        const FlSpot(2, 5),
-                        const FlSpot(3, 0),
-                        const FlSpot(4, 5),
-                        const FlSpot(5, 3),
-                        const FlSpot(6, 4),
-                        const FlSpot(7, 4),
-                        const FlSpot(8, 3),
-                        const FlSpot(9, 5),
-                      ],
-                      isCurved: false,
-                      colors: [Colors.green],
-                      dotData: FlDotData(show: true),
-                      belowBarData: BarAreaData(show: false),
-                    ),
-                  ],
                 ),
               ),
-            ),
-          ],
+              const Divider(
+                color: AppColors.white,
+                thickness: 1.5,
+              ),
+              Row(
+                children: [
+                  Text(
+                    SimpleEmoji(character: "😀").character,
+                    style: AppTextStyles.kHeading3TextStyle,
+                  ),
+                  Text(
+                    "  Team Mood  ",
+                    style: AppTextStyles.kBody17RegularTextStyle
+                        .copyWith(color: AppColors.white),
+                  ),
+                  const Expanded(
+                    child: Divider(
+                      color: AppColors.white,
+                      thickness: 1.5,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              ConstantContainer(
+                  height: Get.height * .13,
+                  width: Get.width,
+                  child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            RichText(
+                              text: TextSpan(
+                                text: ' " ',
+                                style: AppTextStyles.kHeading1TextStyle
+                                    .copyWith(color: AppColors.primary),
+                                children: <TextSpan>[
+                                  TextSpan(
+                                    text: 'The team is feeling good\n     today',
+                                    style: AppTextStyles.kBody20SemiboldTextStyle
+                                        .copyWith(color: AppColors.white),
+                                  ),
+                                  TextSpan(
+                                    text: ' "',
+                                    style: AppTextStyles.kHeading1TextStyle
+                                        .copyWith(color: AppColors.primary),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ]))),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                height: Get.height*0.25,
+                child: LineChart(
+                  LineChartData(
+                    gridData: FlGridData(show: true),
+                    titlesData: FlTitlesData(show: true),
+                    borderData: FlBorderData(
+                      show: true,
+                      border: Border.all(color: AppColors.white90, width: 1),
+                    ),
+                    minX: 0,
+                    maxX: 9,
+                    minY: 0,
+                    maxY: 5,
+                    lineBarsData: [
+                      LineChartBarData(
+                        spots: [
+                          const FlSpot(0, 4),
+                          const FlSpot(2, 5),
+                          const FlSpot(3, 0),
+                          const FlSpot(4, 5),
+                          const FlSpot(5, 3),
+                          const FlSpot(6, 4),
+                          const FlSpot(7, 4),
+                          const FlSpot(8, 3),
+                          const FlSpot(9, 5),
+                        ],
+                        isCurved: false,
+                        colors: [Colors.green],
+                        dotData: FlDotData(show: true),
+                        belowBarData: BarAreaData(show: false),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/services/apis/emoi_api.dart
+++ lib/services/apis/emoi_api.dart
@@ -5,7 +5,10 @@ import 'package:http/http.dart' as http;
 import '../../screen/models/emoji_model.dart';
 
 Future<List<SimpleEmoji>> fetchEmojiList() async {
-  final response = await http.get(Uri.parse('https://emoji-api.com/emojis?access_key=5a77e9b0b489eb91cbfc2cc807ffec18911fee61'));
+  final response = await http.get(Uri.parse(
+
+      'https://emoji-api.com/emojis?access_key=5a77e9b0b489eb91cbfc2cc807ffec18911fee61'
+  ));
 
   if (response.statusCode == 200) {
     final List<dynamic> responseData = json.decode(response.body);
